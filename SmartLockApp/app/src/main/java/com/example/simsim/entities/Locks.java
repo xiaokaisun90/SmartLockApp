b1f2094e61104880da0f5c8992ecacf9eb1cd72c
NeoNil <neo.q.zhang@gmail.com>
2015-07-21 14:10:36 -0400

    20150721141000

---
+++ SmartLockApp/app/src/main/java/com/example/simsim/entities/Locks.java
@@ -0,0 +1,18 @@
+package com.example.simsim.entities;
+
+/**
+ * Created by Qiuge on 2015/7/21.
+ */
+public class Locks {
+
+    private int lockId;
+    private String description;
+    private boolean isLocked;
+    private double lockPower;
+    private double lockStartAngle;
+    private double lockEndAngle;
+    private String rotationDirection;
+    private double rotationEndPoints;
+
+
+}
