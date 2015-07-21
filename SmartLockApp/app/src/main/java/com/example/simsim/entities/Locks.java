2785b86b8c6583127cc47eb2e2a03f1cbf00e6df
NeoNil <neo.q.zhang@gmail.com>
2015-07-21 14:15:29 -0400

    update 20150721 141500

--- SmartLockApp/app/src/main/java/com/example/simsim/entities/Locks.java
+++ SmartLockApp/app/src/main/java/com/example/simsim/entities/Locks.java
@@ -14,5 +14,68 @@ public class Locks {
     private String rotationDirection;
     private double rotationEndPoints;
 
+    public double getLockPower() {
+        return lockPower;
+    }
+
+    public void setLockPower(double lockPower) {
+        this.lockPower = lockPower;
+    }
+
+    public int getLockId() {
+        return lockId;
+    }
+
+    public void setLockId(int lockId) {
+        this.lockId = lockId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+    public void setIsLocked(boolean isLocked) {
+        this.isLocked = isLocked;
+    }
+
+    public double getLockStartAngle() {
+        return lockStartAngle;
+    }
+
+    public void setLockStartAngle(double lockStartAngle) {
+        this.lockStartAngle = lockStartAngle;
+    }
+
+    public double getLockEndAngle() {
+        return lockEndAngle;
+    }
+
+    public void setLockEndAngle(double lockEndAngle) {
+        this.lockEndAngle = lockEndAngle;
+    }
+
+    public String getRotationDirection() {
+        return rotationDirection;
+    }
+
+    public void setRotationDirection(String rotationDirection) {
+        this.rotationDirection = rotationDirection;
+    }
+
+    public double getRotationEndPoints() {
+        return rotationEndPoints;
+    }
+
+    public void setRotationEndPoints(double rotationEndPoints) {
+        this.rotationEndPoints = rotationEndPoints;
+    }
 
 }
